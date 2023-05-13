71cad40c6385a665438080211c95220fb39f0851
HarshVerma98 <49474526+HarshVerma98@users.noreply.github.com>
2023-05-13 09:05:58 +0530

    Core Data stack setup done

--- BudgetTrack/SceneDelegate.swift
+++ BudgetTrack/SceneDelegate.swift
@@ -6,20 +6,26 @@
 //
 
 import UIKit
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    lazy var persistentContainer: NSPersistentContainer = {
+       let cont = NSPersistentContainer(name: "BudgetModel")
+        cont.loadPersistentStores { customDescription, failure in
+            if let fault = failure {
+                fatalError(fault.localizedDescription)
+            }
+        }
+        return cont
+    }()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = ViewController(persistentContainer: persistentContainer)
         window?.makeKeyAndVisible()
     }
 
--- BudgetTrack/ViewController.swift
+++ BudgetTrack/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    private var persistentContainer: NSPersistentContainer
+    
+    init(persistentContainer: NSPersistentContainer) {
+        self.persistentContainer = persistentContainer
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .brown
+        
         // Do any additional setup after loading the view.
     }
 
